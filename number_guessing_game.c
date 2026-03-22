355adb4a1e96eaffaf99267b4a3c8792ec354eb4
Ketan_Pipania <ketanpipania@gmail.com>
2026-03-22 11:30:08 +0530

    Add files via upload
    This commit includes a simple number guessing game written in C. 
    The program generates a random number and asks the user to guess it.

---
+++ number_guessing_game.c
@@ -0,0 +1,29 @@
+// game for guess a no bw 1to 100
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main()
+{
+
+    int number,guess,nguesses=1;
+    srand(time(0));
+     number = rand()%100 + 1;//generates a random no bw 1 and 100
+     //printf("The number is %d\n",number);
+   
+     // keep running the loop untill the no is guessed
+     do{
+        printf("Guess the number between 1 to 100 !\n");
+        scanf("%d",&guess);
+        if(guess>number){
+            printf("Lower number please!\n");}
+            else if(guess<number)
+            {printf("Higher number please!\n");}
+        else
+        {printf("You guessed it in %d attempts!\n",nguesses);}
+        nguesses++;
+
+
+     }while(guess!=number );
+
+   return 0; 
+    }
